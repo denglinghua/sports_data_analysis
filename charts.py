40a389194431f063e5de275d344c94e5cd447ca5
deng <denglinghua@outlook.com>
2021-08-24 20:56:11 +0800

    chart sytle

--- charts.py
+++ charts.py
@@ -7,15 +7,21 @@ from group import Group
 
 
 def draw_group_chart(group):
+    yaxis_opts = None
+    if (group.ytitle):
+        yaxis_opts = opts.AxisOpts(axislabel_opts=opts.LabelOpts(
+            formatter="{value} /%s" % group.ytitle))
+
     axis_values = group.get_axis_values()
     c = (Bar(init_opts=opts.InitOpts(theme=ThemeType.ROMA))
          .add_xaxis(axis_values[0])
-         .add_yaxis("", axis_values[1])
-         .set_global_opts(title_opts=opts.TitleOpts(title=group.title, subtitle=""))
-         #.render('chart_html/' + group.title + ".html")
-    )
+         .add_yaxis("", axis_values[1], itemstyle_opts=opts.ItemStyleOpts(color='purple'))
+         .set_global_opts(title_opts=opts.TitleOpts(title=group.title, subtitle=""), yaxis_opts=yaxis_opts)
+         # .render('chart_html/' + group.title + ".html")
+         )
 
     return c
+
 
 def draw_groups_chart(title, groups):
     page = Page()
