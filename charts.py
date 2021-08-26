85fbc26573cb24007bdbbf442c84042cdacff087
denglinghua <denglinghua@aobi.com>
2021-08-26 14:42:37 +0800

    add customer native js formatter

--- charts.py
+++ charts.py
@@ -3,6 +3,7 @@ from pyecharts import options as opts
 from pyecharts.charts import Bar, Page
 from pyecharts.faker import Faker
 from pyecharts.globals import ThemeType
+from pyecharts.commons import utils
 from group import Group
 
 
@@ -14,11 +15,11 @@ def draw_group_chart(group):
     c = (Bar(init_opts=opts.InitOpts(theme=ThemeType.ROMA))
          .add_xaxis(axis_values[0])
          .add_yaxis("", axis_values[1], itemstyle_opts=opts.ItemStyleOpts(color='purple'))
-         .set_series_opts(label_opts=opts.LabelOpts(formatter='{c} %s' % ytitle))
+         .set_series_opts(label_opts=opts.LabelOpts(formatter=__get_formatter(group.title, ytitle)))
          .set_global_opts(title_opts=opts.TitleOpts(title=group.title, subtitle=""),
                           yaxis_opts=opts.AxisOpts(is_show=False))
          )
-
+    
     return c
 
 
@@ -28,3 +29,23 @@ def draw_groups_chart(title, groups):
     for group in groups:
         page.add(draw_group_chart(group))
     page.render('chart_html/all.html')
+
+__mins_to_hm_formatter = """function (params) {
+        mins = params.value;
+        h = Math.floor(mins / 60); m = mins % 60;
+        r = '';
+        if (h > 0) r += (h + '小时');
+        if (m > 0) r += (m + '分');
+        return r;
+    }
+"""
+
+__formatters = {
+    "三项耗时" : __mins_to_hm_formatter    
+}
+
+def __get_formatter(title, ytitle):
+    if title in __formatters:
+        return utils.JsCode(__formatters[title])
+    else:
+        return '{c} %s' % ytitle
